7a6900b37954e1f8d8df10aabafb4dd3abba8750
lehelen19 <le.helen19@gmail.com>
2023-05-15 10:20:57 -0700

    Add login and check-token routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const usersController = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 const router = express.Router();
 
+router.get('/check-token', ensureLoggedIn, usersController.checkToken);
+
 router.post('/', usersController.create);
+
+router.post('/login', usersController.login);
 
 module.exports = router;
