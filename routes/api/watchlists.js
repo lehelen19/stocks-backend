00d6e9a68fbf044f1c66be0cd2fdaefd089da2f9
lehelen19 <le.helen19@gmail.com>
2023-05-15 11:02:45 -0700

    Remove watchlist ensure login until finished testing

--- routes/api/watchlists.js
+++ routes/api/watchlists.js
@@ -4,6 +4,7 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 const router = express.Router();
 
-router.post('/', ensureLoggedIn, watchlistsController.create);
+// add ensureLoggedIn when not testing
+router.post('/', watchlistsController.create);
 
 module.exports = router;
