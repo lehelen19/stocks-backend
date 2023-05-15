befc07251fa321c47629fcf438e6655f7b91c865
lehelen19 <le.helen19@gmail.com>
2023-05-15 10:56:39 -0700

    Set up post watchlists route

---
+++ routes/api/watchlists.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const watchlistsController = require('../../controllers/api/watchlists');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+const router = express.Router();
+
+router.post('/', watchlistsController.create);
+
+module.exports = router;
