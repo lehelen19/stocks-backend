42593328f35494c198449ad88730ffd2132e21af
lehelen19 <le.helen19@gmail.com>
2023-05-15 10:57:59 -0700

    Ensure logged in for create watchlists

--- routes/api/watchlists.js
+++ routes/api/watchlists.js
@@ -4,6 +4,6 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 const router = express.Router();
 
-router.post('/', watchlistsController.create);
+router.post('/', ensureLoggedIn, watchlistsController.create);
 
 module.exports = router;
